e9112c45f6207f193a8cd273ce775c23b070d6c4
Frankie Bukenya <frankie.bukenya@gmail.com>
2019-04-22 06:18:29 +0300

    Add Color Randomization for simon game

---
+++ game.js
@@ -0,0 +1,11 @@
+var gamePattern = [];
+var buttonColors = ['red', 'blue', 'green', 'yellow'];
+
+// Random number generating function
+function nextSequence() {
+  var randomNumber = Math.floor(Math.random() * 4);
+  var randomChosenColor = buttonColors[randomNumber];
+  gamePattern.push(randomChosenColor);
+}
+
+nextSequence();
