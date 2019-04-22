9f013c873a656b39ce33d640307849fe797cfe13
Frankie Bukenya <frankie.bukenya@gmail.com>
2019-04-22 14:50:28 +0300

    Add Animation And Sound  to game

--- game.js
+++ game.js
@@ -1,9 +1,20 @@
-var gamePattern = [];
 var buttonColors = ['red', 'blue', 'green', 'yellow'];
+var randomNumber = Math.floor(Math.random() * 4);
+var gamePattern = [];
 
 // Random number generating function
 function nextSequence() {
   var randomNumber = Math.floor(Math.random() * 4);
   var randomChosenColor = buttonColors[randomNumber];
   gamePattern.push(randomChosenColor);
+
+  // Flashing animation and sounds for colours
+
+  $('#' + randomChosenColor)
+    .fadein(100)
+    .fadeout(100)
+    .fadein(100);
+
+  var audio = new Audio('sounds/' + randomChosenColor + '.mp3');
+  audio.play();
 }
